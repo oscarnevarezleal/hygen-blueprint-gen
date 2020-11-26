ffad920f4a11cb19526ef51437b69a2bf8649475
Oscar Nevarez <fu.wire@gmail.com>
2020-11-26 17:37:56 -0600

    RC 1.0.82

--- build.js
+++ build.js
@@ -1,2 +1,4 @@
 const {builder} = require('./dist/blueprint_gen.js').default.default;
-builder.buildBlueprintFile();
+builder.buildBlueprintFile()
+    .then(r => console.log('Files written!'))
+    .catch(err => console.error(err))
