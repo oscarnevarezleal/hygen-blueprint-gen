d7d4a7dd3021a42e661d4d654b43d1cfb0814dec
Oscar Nevarez <fu.wire@gmail.com>
2020-11-26 17:47:46 -0600

    Include CI/CD for RC 1.0.82

--- build.js
+++
@@ -1,4 +0,0 @@
-const {builder} = require('./dist/blueprint_gen.js').default.default;
-builder.buildBlueprintFile()
-    .then(r => console.log('Files written!'))
-    .catch(err => console.error(err))
