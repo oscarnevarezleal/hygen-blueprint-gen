c0ce69ad5173adf45e813f758b4b68eb4ec238d1
Oscar Nevarez <fu.wire@gmail.com>
2020-11-26 18:59:49 -0600

    Debug 1.0.83

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 const lib = require('blueprint-gen').default;
+console.log('<>', lib)
 lib.builder.buildBlueprintFile()
     .then(r => console.log('Files written!'))
     .catch(err => console.error(err))
